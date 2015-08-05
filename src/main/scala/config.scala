cf0a9c695797d49fdfd470669d1375ac7716a558
Simon Hafner <shafner@oaganalytics.com>
2015-08-05 14:07:46 +0200

    small copyFromLocalFile example

--- src/main/scala/config.scala
+++ src/main/scala/config.scala
@@ -2,10 +2,18 @@ package com.oaganalytics
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
+import au.com.cba.omnia.permafrost.hdfs._
 
 object Config {
   val conf = new Configuration()
-  conf.addResource(new Path(getClass().getResource("/conf/core-site.xml")))
-  conf.addResource(new Path(getClass().getResource("/conf/hdfs-site.xml")))
-  conf.addResource(new Path(getClass().getResource("/conf/mapred-site.xml")))
+  conf.addResource(new Path(getClass().getResource("/conf/core-site.xml").toURI))
+  conf.addResource(new Path(getClass().getResource("/conf/hdfs-site.xml").toURI))
+  conf.addResource(new Path(getClass().getResource("/conf/mapred-site.xml").toURI))
+}
+
+object Test {
+  def main(args: Array[String]) {
+    println(Config.conf)
+    Hdfs.copyFromLocalFile(new java.io.File("/tmp/testfile"), new org.apache.hadoop.fs.Path("/testfile")).run(Config.conf)
+  }
 }
