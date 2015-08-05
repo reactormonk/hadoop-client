df51ba6cf0f765765e0f0140d7c0dc7268c552ce
Simon Hafner <shafner@oaganalytics.com>
2015-08-05 14:45:49 +0200

    automatic config pickup

--- src/main/scala/config.scala
+++ src/main/scala/config.scala
@@ -6,14 +6,10 @@ import au.com.cba.omnia.permafrost.hdfs._
 
 object Config {
   val conf = new Configuration()
-  conf.addResource(new Path(getClass().getResource("/conf/core-site.xml").toURI))
-  conf.addResource(new Path(getClass().getResource("/conf/hdfs-site.xml").toURI))
-  conf.addResource(new Path(getClass().getResource("/conf/mapred-site.xml").toURI))
 }
 
 object Test {
   def main(args: Array[String]) {
-    println(Config.conf)
     Hdfs.copyFromLocalFile(new java.io.File("/tmp/testfile"), new org.apache.hadoop.fs.Path("/testfile")).run(Config.conf)
   }
 }
