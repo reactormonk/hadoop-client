171392aa15eb3f4b6a57c8a42571a1fdd390a005
Simon Hafner <shafner@oaganalytics.com>
2015-08-05 15:08:45 +0200

    moved config files

--- src/main/scala/config.scala
+++ src/main/scala/config.scala
@@ -4,12 +4,11 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import au.com.cba.omnia.permafrost.hdfs._
 
-object Config {
-  val conf = new Configuration()
-}
-
 object Test {
   def main(args: Array[String]) {
-    Hdfs.copyFromLocalFile(new java.io.File("/tmp/testfile"), new org.apache.hadoop.fs.Path("/testfile")).run(Config.conf)
+    val conf = new Configuration()
+    println(conf)
+    println(conf.get("fs.defaultFS"))
+    println(Hdfs.copyFromLocalFile(new java.io.File("/tmp/testfile"), new org.apache.hadoop.fs.Path("/testfile")).run(conf))
   }
 }
